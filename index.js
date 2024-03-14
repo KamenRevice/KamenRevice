7b072694b5a33cc0bfedbd8deab6f3e9a3eaa412
Revice <36665957+KamenRevice@users.noreply.github.com>
2024-03-14 10:54:20 +0800

    Update index.js
    add snake animation instruction🐍

--- index.js
+++ index.js
@@ -23,7 +23,11 @@ const readme = `\
 ⏰ Updated on ${new Date().toUTCString()}
 
 ---
-![snake](https://raw.githubusercontent.com/KamenRevice/KamenRevice/output/github-contribution-grid-snake.svg)
+
+🦕🦕🦕 There will be a 🐍 animation below, if it doesn't show up, check for scientific internet access.
+🐍🐍🐍 下面会有一个贪吃蛇🐍的动画，如果没有显示，请检查是否科学上网。
+
+![🐍🐍🐍](https://raw.githubusercontent.com/KamenRevice/KamenRevice/output/github-contribution-grid-snake.svg)
 
 [![Progress Bar & Snake](https://github.com/KamenRevice/KamenRevice/actions/workflows/main.yml/badge.svg)](https://github.com/KamenRevice/KamenRevice/actions/workflows/main.yml)\
 `
